c13b72cdb2b2d8ae9ad648bd46bca71f89b34ec4
Sanjay24.Patel <Sanjay24.Patel@ril.com>
2024-04-16 15:42:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 4001;
+const routes = require('./route.js');
+
+app.use(express.json());
+
+app.use('', routes);
+
+mongoose.connect('mongodb://localhost:27017/user-service', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((error) => {
+    console.log(error);
+});
+
+
