1e22c9700f6a052a9389b3ef6544a891ebb0e5da
sanjay-singh-patel <sanjupatel11112000@gmail.com>
2024-04-17 11:32:08 +0530

    Add product route

--- route.js
+++ route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoute = require('./user/user.route.js');
+const productRoute = require('./product/product.route.js');
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ const allRoutes = [
   {
     path: '/users',
     route: userRoute,
+  },
+  {
+    path: '/products',
+    route: productRoute,
   }
 ];
 
